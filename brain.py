901c050d81435a9b0b58cd9f738e935f05074967
Bram Bout <bram@bbflight.com>
2021-12-09 19:46:59 -0800

    Fixed square flight path, added plots

--- brain.py
+++ brain.py
@@ -1,11 +1,11 @@
 import socket
 import struct
 import threading
-import time
+
+import matplotlib.colors
+import matplotlib.pyplot as plt
 from typing import Callable
 
-from flask import Flask, request, Response
-import requests
 import logging
 
 from drone_data import DroneData
@@ -17,6 +17,8 @@ UDP_PORT = 1234  # random number
 
 HOVER_Z_THRESHOLD = 0.5
 HOVER_Z = HOVER_Z_THRESHOLD * 1.1  # with safety margin
+DRONE1_X_OFFSET = 0.5  # relative to Drone 0 at start
+DRONE1_Y_OFFSET = 0  # relative to Drone 0 at start
 
 drone_data_list = [DroneData(), DroneData()]  # global variable
 
@@ -25,6 +27,8 @@ drone_data_list[1].ip = '192.168.86.82'  # TODO change ip address to drone addre
 BRAIN_PORT = '8100'
 CLIENT0_PORT = '8080'
 CLIENT1_PORT = '8000'
+COLORS = ['r', 'g']
+tracks = [[], []]  # (x,y,z, color) as a list for each drone
 
 
 def drone_data_listener(wrap_up: Callable[[], bool]):
@@ -51,12 +55,23 @@ def drone_data_listener(wrap_up: Callable[[], bool]):
                         drone_data.start_x = x
                         drone_data.start_y = y
                         drone_data.start_z = z
+                    log_drone_positions()
                     recalculate(wrap_up())
                     if wrap_up():
                         break
                 else:
                     logging.warning(f'Received only {len(data)} bytes: {data}')
     logging.info('Socket listener finished')
+
+
+def log_drone_positions():
+    for i in range(2):
+        x_offset = DRONE1_X_OFFSET if i == 1 else 0  # logging real position
+        y_offset = DRONE1_Y_OFFSET if i == 1 else 0  # logging real position
+        drone = drone_data_list[i]
+        tracks[i].append((drone.real_x() + x_offset,
+                          drone.real_y() + y_offset,
+                          drone.real_z(), COLORS[i]))
 
 
 def recalculate(wrap_up: bool):
@@ -90,6 +105,7 @@ def set_targets_for_square_mirror(drone0, drone1):
     set_target_for_square(drone0)
     drone1.set_target(drone0.target_x, drone0.target_y, drone0.target_z)
 
+target_quadrant = 0
 
 def set_target_for_square(drone: DroneData):
     """
@@ -97,34 +113,25 @@ def set_target_for_square(drone: DroneData):
     :param drone:
     """
     assert drone.real_z() > HOVER_Z_THRESHOLD
+    global target_quadrant
     radius = 1  # drone will fly to corners of (radius, radius) square
-    setpoint = radius * 1.2  # drone will fly beyond those corners
+    setpoint = radius * 1.3  # drone will fly beyond those corners
     x = drone.real_x()
     y = drone.real_y()
     # quadrant is 0, 1, 2, 3 from top right clockwise
-    current_quadrant = 0 if x > 0 and y > 0 else (1 if x > 0 and y <= 0 else (2 if x <=0 and y <= 0 else 3))
+    current_quadrant = 0 if x > 0 and y > 0 else (1 if x > 0 and y <= 0 else (2 if x <= 0 and y <= 0 else 3))
     inside_square = abs(x) < radius and abs(y) < radius
-    at_corner = abs(x) > (radius + setpoint) / 2 and abs(y) > (radius + setpoint) /2
-    target_quadrant = (current_quadrant + 1) % 4 if at_corner else current_quadrant
+    at_corner = abs(x) > (radius + setpoint) / 2 and abs(y) > (radius + setpoint) / 2
     if at_corner:
+        target_quadrant = (current_quadrant + 1) % 4
         logging.info(f'At corner of quadrant {current_quadrant} moving to {target_quadrant}')
     if inside_square:
+        # initially, move to corner of quadrant 0
         target_quadrant = 0
     xy = [(setpoint, setpoint), (setpoint, -setpoint), (-setpoint, -setpoint), (-setpoint, setpoint)]
     target_xy = xy[target_quadrant]
     drone.set_target(target_xy[0], target_xy[1], HOVER_Z)
 
-def set_targets_for_circle(drone0, drone1):
-    """
-    Set drone targets such that drone1 circles drone0 at the same altitude
-
-    Expects drone0 to start at real world (0,0) and drone1 at real world (1,0)
-    """
-    drone1_start_x = 1  # implies real world (1, 0) start point
-    logging.info('setting target for both drones')
-    assert drone0.real_z() > HOVER_Z_THRESHOLD
-    drone0.set_target(1, 2, 1)
-    drone1.set_target(drone0.target_x + 1, drone0.target_y, drone0.target_z)
 
 def send_to_drone(drone_data: DroneData, id):
     """
@@ -154,6 +161,23 @@ def main():
     value = wrap_up = True
     t1.join()
     logging.info('Finished')
+    # plot both tracks on one map
+    xs = []
+    ys = []
+    zs = []
+    cs = []
+    for i in range(2):
+        xs.extend([item[0] for item in tracks[i]])
+        ys.extend([item[1] for item in tracks[i]])
+
+        # zs = ([item[2] for item in tracks[i]])
+        cs.extend([item[3] for item in tracks[i]])
+    plt.scatter(xs, ys, c=cs, alpha=0.8)
+    plt.show()
+    # print(tracks)
+    # print(xs)
+    # print(ys)
+    # print(cs)
 
 
 if __name__ == '__main__':
